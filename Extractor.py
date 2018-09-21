35e90da7ae3357063aab2070b1d41a3317af3835
KonstantinZhyl <Konstantinzhyl@gmail.com>
2018-09-21 13:10:07 +0300

    Update Extractor.py

--- Extractor.py
+++ Extractor.py
@@ -7,12 +7,12 @@ file = open(sys.argv[-2], "r")
 content = file.read()
 file.close()
 
-tags = re.findall(r"<tr>[\s\S]+?data-event-category=\"Job Link\"[\s\S]+?</tr>", content)
+tags = re.findall(r"<tr>[\s\S]+?data-event-category=\"Job Link\"[\s\S]+?</tr>", content)   # finds rows in table
 
 vacancies = []
 for i in tags:
-    tmp = re.findall(r">\s*([^<>]*?\w[^<>]*?)\s*?<" , i)
-    for i in range(3, len(tmp)):
+    tmp = re.findall(r">\s*([^<>]*?\w[^<>]*?)\s*?<" , i)        # finds info in rows
+    for i in range(3, len(tmp)):                                # in case of multiple locations for that vacancy
         vacancies.append([tmp[0], tmp[1], tmp[2], tmp[i]])
 
 with open(sys.argv[-1], 'w', newline='') as csvfile:
